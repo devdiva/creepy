1c68be7dd32f2e58b21e4a7ed5e3217d27c2dc6a
Hari Khalsa <hari@devdiva.com>
2013-04-09 20:47:04 -0700

    My creepy app with a home contoller and devise initial files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Creepy::Application.config.secret_token = '0992c8e3eb48282b62971dea352f96b23bfd3ef427c0de0e636b71bd5bebc4e5e755be4217bdae5b638daae8e25c1cfde07f379eb618aaad99b0e18f215b6e20'
